0e3d4bf97c611ca1cfad8b4f96cf5476949f9c8a
imazuchova <majerova265@gmail.com>
2024-09-14 21:17:44 +0200

    AoC day 1 solution

---
+++ day1/src/main/java/advent/of/code/Main.java
@@ -0,0 +1,12 @@
+package advent.of.code;
+
+import advent.of.code.day1.Day1;
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Day1.class, args);
+    }
+}
